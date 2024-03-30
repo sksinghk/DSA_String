96c0302978f09f460f7f4e05f6db808f22c06993
KaushalinSocial <154751081+sksinghk@users.noreply.github.com>
2024-03-30 11:38:54 +0530

    Add files via upload

---
+++ substrings.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.*;
+public class substringss2 {
+    public static void main(String[] args) {
+        String str ="abcd";
+        for (int i=0;i<=3;i++){
+            for (int j=i+1;j<=4;j++){
+                System.out.print(" "+str.substring(i,j));
+
+            }
+
+        }
+
+    }
+}
