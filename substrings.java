f0273fa792a6d48cc7383d958f889862c5c83821
Saadsayyed527 <137900721+Saadsayyed527@users.noreply.github.com>
2024-03-31 17:16:42 +0530

    Update substrings.java
    length() function add kiya      -- saad

--- substrings.java
+++ substrings.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class substringss2 {
     public static void main(String[] args) {
         String str ="abcd";
-        for (int i=0;i<=3;i++){  // how can you use the 3 
-            for (int j=i+1;j<=4;j++){
+        for (int i=0;i<=str.length()-1;i++){  // how can you use the 3 
+            for (int j=i+1;j<=str.length();j++){
                 System.out.print(" "+str.substring(i,j));
 
             }
